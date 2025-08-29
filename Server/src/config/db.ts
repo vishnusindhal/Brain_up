4e0b2d21098d040e99b6f3d0b472c2084c88d214
vishnu teli <vishnusindhal5040@gmail.com>
2025-08-30 00:18:23 +0530

    adding the mongo url in the .env filr

--- Server/src/config/db.ts
+++ Server/src/config/db.ts
@@ -1,8 +1,14 @@
 import mongoose from "mongoose"
 
+// Load environment variables
 const dbConnect = () => {
+  const dbUrl = process.env.DBurl; // Get DBurl from .env
+  if (!dbUrl) {
+    console.log("Database URL not found in environment variables");
+    return;
+  }
   mongoose
-    .connect("mongodb+srv://vishnusindhal5040:Vishnu$2151@eventopiadb.prvfrwe.mongodb.net/Brain_up")
+    .connect(dbUrl)
     .then(() => {
       console.log("Connected Successfully")
     })
