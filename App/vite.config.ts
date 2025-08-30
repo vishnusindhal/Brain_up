82dcd9d8a06f92aec7125d3d7d4eb96168db6bf2
vishnu teli <vishnusindhal5040@gmail.com>
2025-08-30 14:58:36 +0530

    edit the vite.config.js

--- App/vite.config.ts
+++ App/vite.config.ts
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: true,
+    cors: {
+      origin: [
+        // 'https://brain-up-backend.onrender.com',
+        // 'http://localhost:5000'
+      ],
+      credentials: true
+    },
+    allowedHosts: [
+      // 'brain-up-frontend.onrender.com',
+      // 'brain-up-dashboard.onrender.com',
+      // 'brain-up-backend.onrender.com',
+      // '.onrender.com',
+    ]
+  }
 })
